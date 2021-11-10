c5ffd54483f2536fab32f059296159d93174e1c9
SodaCris <18463922396@163.com>
2021-11-10 09:29:19 +0800

    adding files

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BankAccountNS;
+using System;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("tests!");
+        }
+    }
+}
