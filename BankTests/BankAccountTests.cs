5c5c4e52f079fd19c19797baf44fbe70fa138e4f
SodaCris <18463922396@163.com>
2021-11-10 09:58:19 +0800

    adding tests

--- BankTests/BankAccountTests.cs
+++ BankTests/BankAccountTests.cs
@@ -8,9 +8,42 @@ namespace BankTests
     public class BankAccountTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Debit_WithValidAmount_UpdatesBalance()
         {
-            Console.WriteLine("tests!");
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = 4.55;
+            double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act
+            account.Debit(debitAmount);
+
+            // Assert
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
+        public void Debit_WhenAmountIsLessThanZero_ShouldThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+
+            // Act and assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
+        }
+        [TestMethod]
+        public void Debit_WhenAmountIsMoreThanBalance_ShoundThrowArgumentOutOfRange()
+        {
+            // Arrange
+            double beggingBalance = 11.99;
+            double debitAmount = 12.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beggingBalance);
+
+            // Act and assert
+            Assert.ThrowsException<System.ArgumentOutOfRangeException>(() => account.Debit(debitAmount));
         }
     }
 }
